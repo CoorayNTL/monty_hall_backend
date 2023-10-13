df7f8861a7e4748635cb8bda1deb259d42292452
CoorayNTL <tl685998@gmail.com>
2023-10-13 20:06:18 +0530

    try to fix bug controller

---
+++ WebApplication1/WebApplication1/WebApplication1/MontyHallSimulation.cs
@@ -0,0 +1,8 @@
+﻿namespace MontyHallAPI.Models
+{
+    public class MontyHallSimulation //handel the major input parmeters in the problem
+    {
+        public int NumberOfSimulations { get; set; }
+        public bool SwitchDoor { get; set; }
+    }
+}
